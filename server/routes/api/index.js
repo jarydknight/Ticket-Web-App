3b43326952624810f6e5e849964ff4c00dacdf36
Jaryd Knight <jaryd.knight@hotmail.com>
2024-01-26 14:59:49 -0500

    update index.js file in routes to include new routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const ticketRoutes = require("./ticketRoutes");
+const ticketBucketRoutes = require("./ticketBucketRoutes");
+const ticketCommentRoutes = require("./ticketCommentRoutes");
 
 router.use('/users', userRoutes);
 router.use('/tickets', ticketRoutes);
+router.use('/comment', ticketCommentRoutes);
+router.use('/comment', ticketCommentRoutes);
 
 module.exports = router;
